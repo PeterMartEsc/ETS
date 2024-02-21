a22f73f015b7bfd54a5dd65da5f54bab63e6d599
PeterMartEsc <petermartesc@gmail.com>
2024-02-21 15:06:29 +0000

    Se actualizan los cambios

--- Junit-Test/junit-test/junit-test/src/main/java/es/iespuerto/ets/negocio/BibliotecaNegocio.java
+++ Junit-Test/junit-test/junit-test/src/main/java/es/iespuerto/ets/negocio/BibliotecaNegocio.java
@@ -1,10 +1,25 @@
 package es.iespuerto.ets.negocio;
 
 import es.iespuerto.ets.modelo.Biblioteca;
+import es.iespuerto.ets.modelo.Libro;
+
+import java.util.List;
 
 public class BibliotecaNegocio {
 
     Biblioteca biblioteca;
 
 
+    public BibliotecaNegocio(int id){
+        biblioteca = new Biblioteca(id);
+    }
+
+    public BibliotecaNegocio(int id, String nombre, String direccion){
+        biblioteca = new Biblioteca(id, nombre, direccion);
+    }
+
+    public List<Libro> obtenerLibros(){
+
+        return biblioteca.getLibros();
+    }
 }
